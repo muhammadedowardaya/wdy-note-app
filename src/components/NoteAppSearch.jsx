dadcd0c52928a48d89ba0b38d8eaa162f7f9e9ee
Muhammad Edo Wardaya <muhammadedowarday4@gmail.com>
2023-09-14 15:12:43 +0700

    perbaiki gambar icon di NoteAppSearch

--- src/components/NoteAppSearch.jsx
+++ src/components/NoteAppSearch.jsx
@@ -15,7 +15,7 @@ class NoteAppSearch extends React.Component {
 		return (
 			<div className="note-app__search">
 				<div className="nav-brand">
-					<img src="/public/quill-drawing-a-line.png" alt="icon pen" />
+					<img src="./public/quill-drawing-a-line.png" alt="icon pen" />
 					<h1>Notes</h1>
 				</div>
 				<input
